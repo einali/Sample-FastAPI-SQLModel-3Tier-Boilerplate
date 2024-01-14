16f1f5ef8a3518c645ac314b7cee5c59912ea7ce
m.einali <einali@gmail.com>
2024-01-14 17:46:43 +0330

    enum added

--- package_name/models/dtos/education/student_dto.py
+++ package_name/models/dtos/education/student_dto.py
@@ -9,5 +9,5 @@ from package_name.models.dtos.base_dto import BasicDTO
 class StudentDTOCreate(SQLModel):
     first_name: str
     last_name: str
-    nationality_type: int
+    nationality_type: str
 
--- package_name/models/enums/personnel/nationality_type.py
+++ package_name/models/enums/personnel/nationality_type.py
@@ -2,11 +2,16 @@ from enum import unique, Enum, StrEnum
 
 
 @unique
-class NationalityType(Enum):
-    IRAN =  1648195
-    FOREIGN =  0
+class NationalityType(StrEnum):
+    IRAN =  "IRAN"
+    FOREIGN =  "FOREIGN"
 
 
+    def get_area(self)->int:
+        if self==self.IRAN:
+            return 168195
+        elif self==self.FOREIGN:
+            return 0
 
 
 if __name__ == '__main__':
